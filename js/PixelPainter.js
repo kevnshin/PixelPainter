9f6be539d7dca9ccfa2bfc6054679588046fa287
Kevin Shin <kevnshin@gmail.com>
2014-12-06 18:43:04 -1000

    Moved PixelPainter class to separate file. Made methods for other functionalities

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,12 +1,78 @@
-var main = function(){
+//PixelPainter class
+function PixelPainter (width, height) {
 
-  
+  this.width = width;
+  this.height = height;
 
+}
 
+PixelPainter.prototype.render = function () {
 
+  var app = $("<div>", {
+    class: "app_contents"
+  });
+  // app.append(this.color_pallette());
+  app.append(this.build_grid());
+  this.color_dropper();
 
-
+  return app;
 
 };
 
-$(document).ready(main);
+//This method builds out the grid according 
+//to the width and height provided by the user
+PixelPainter.prototype.build_grid = function () {
+  this.grid = $("<div>", {
+    class: "grid clearfix"
+  });    
+
+  for (var i = 0; i < this.height; i++) {
+
+    var new_pixelrow = $("<div>", {
+      class: "pixel_row clearfix"
+    });
+
+    for (var j = 0; j < this.width ; j++) {
+
+      var new_pixelsquare = $("<div>", {
+        class: "pixel_square"
+      });
+
+      new_pixelrow.append(new_pixelsquare);
+    }
+    this.grid.append(new_pixelrow);
+  }
+  return this.grid;
+}
+
+PixelPainter.prototype.build_colors = function () {
+  
+}
+
+PixelPainter.prototype.color_dropper = function (color) {
+  
+  this.grid.find("div.pixel_square").click(function () {
+    $(this).css('background-color', "#000");// FIX the hardcoded color
+  })
+}
+
+PixelPainter.prototype.color_copier = function () {
+  
+}
+
+PixelPainter.prototype.eraser = function () {
+
+}
+
+PixelPainter.prototype.clear = function () {
+  
+}
+
+
+
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,72 +1,18 @@
 var main = function(){
 
-  var color;
+  // var color;
 
-  var color_pallette = $("<div>", {
-    class: "color"
-  });
-
-  //PixelPainter class
-  function PixelPainter (width, height) {
-
-    this.width = width;
-    this.height = height;
-    this.grid = $("<div>", {
-      class: "grid clearfix"
-    });
-    
-    this.pixel_row = $("<div>", {
-      class: "pixel_row clearfix"
-    });
-    
-    this.pixel_square = $("<div>", {
-      class: "pixel_square"
-    });
-
-  }
-
-  PixelPainter.prototype.render = function () {
-
-    for (var i = 0; i < this.height; i++) {
-      for (var j = 0; j < this.width ; j++) {
-        
-        this.pixel_row.append(this.pixel_square);
-        
-        this.pixel_square = $("<div>", {
-          class: "pixel_square"
-        });
-      }
-      console.log("Row#:"+i);
-      this.grid.append(this.pixel_row);
-      this.pixel_row = $("<div>", {
-        class: "pixel_row clearfix"
-      });
-    }
-
-    return this.grid;
-  };
+  // var color_pallette = $("<div>", {
+  //   class: "color"
+  // });
 
   //when page loads, load the grid
-  var pixelPainter = new PixelPainter(5,5);
+  var pixelPainter = new PixelPainter(15,15);
   $("#container").append(pixelPainter.render());
-
-
 
   // $(color_pallette).click(function () {
   //   color = $(this).css('background-color');
-  // });
-
-  // $(pixel_square).click(function () {
-  //   $(this).css('background-color', color);
-  // })
-
-
-
-
-
-
-
-
+  // }))
 
 
 
