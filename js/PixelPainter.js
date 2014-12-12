109122c6cb151f98f3060c5e79b06bd69c734044
Kevin Shin <kevnshin@gmail.com>
2014-12-11 16:34:01 -1000

    Got cellSize to work as a separate method, refactored code according to code review from Jon to make it cleaner

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,38 +2,33 @@ function PixelPainter (width, height) {
 
   this.width = width;
   this.height = height;
+  this.size = null;
+  this.grid = this.buildGrid();
+  this.palette = this.buildPalette();
+  this.color = null;
+  this.registerEventListeners();
 
 }
 
 //Render should render all elements of the app
 //Just need to run render in app.js, nothing else is needed
 PixelPainter.prototype.render = function () {
-
-  $("#container").addClass("clearfix");
-
-  var app = [this.buildPalette(), this.buildGrid()];
-  this.colorChanger();
-  this.clearAll();
-
-  return app;
-};
+  return [this.palette, this.grid];
+}
 
 //This method builds out the grid according 
 //to the width and height provided by the user
 PixelPainter.prototype.buildGrid = function () {
   
-  this.grid = $("<div>", {
+  var grid = $("<div>", {
     class: "grid clearfix"
   });
-
   //add new rows according to height
   for (var i = 0; i < this.height; i++) {
-
     //make a new row element
     var new_pixelrow = $("<div>", {
       class: "pixel_row clearfix"
     });
-
     //add new columns according to width
     for (var j = 0; j < this.width ; j++) {
 
@@ -41,25 +36,13 @@ PixelPainter.prototype.buildGrid = function () {
       var new_pixelsquare = $("<div>", {
         class: "pixel_square"
       });
-      
-      // if(this.size){
-      //   $("div.pixel_square").css("width", this.size + "px");
-      //   console.log(this.size);
-      // }
-      
-      //add the pixel element to the row
       new_pixelrow.append(new_pixelsquare);
     }
     //add the row to the grid
-    this.grid.append(new_pixelrow);
+    grid.append(new_pixelrow);
   }
-
-
-
-  return this.grid;
-
+  return grid;
 }
-
 
 //Builds the entire palette area: color palette, sampler, buttons
 PixelPainter.prototype.buildPalette = function () {
@@ -87,7 +70,6 @@ PixelPainter.prototype.buildPalette = function () {
       var color_square = $("<div>", {
         class: "color_square " + colors[k]
       });
-
       color_row.append(color_square);
     }
     color_grid.append(color_row);
@@ -106,62 +88,63 @@ PixelPainter.prototype.buildPalette = function () {
     html: "clear all"
   });
 
-  this.palette_container.append(color_grid, color_sampler, eraser, clear);
-  return this.palette_container;
+  return this.palette_container.append(color_grid, color_sampler, eraser, clear);
+
 }
 
-//This method takes care of all color changing listeners
-//The color copier, the eraser, and the color dropper
-PixelPainter.prototype.colorChanger = function () {
+//This method takes care of all event listeners
+//The color copier, the eraser, the color dropper, clear all button
+PixelPainter.prototype.registerEventListeners = function () {
 
-  var color;
+  var self = this;
 
-  //Color copier event listener
+  //Listens for click on any of the colors on the pallette
+  //When clicked, it sets current color as that color
+  //Can't just taret div.color_square becuase it doesn't exist in the context of the page
+  //It exists in context of this.pallette_container
+  //Need to "find" it in there
   this.palette_container.find("div.color_square").click(function () {
-  
-    $("div.color_square").removeClass("selected");
-    $("div.eraser").removeClass("selected");
+    $("div").removeClass("selected");
     $(this).addClass("selected");
-    color = $(this).css('background-color');
-    $("div.color_sampler").css("background-color", color);
+    self.color = $(this).css('background-color');
+    $("div.color_sampler").css("background-color", self.color);
   });
 
-  //Eraser event listener
+  //Listens for click on eraser icon
+  //When clicked, it makes current color ""white" to erase pixels
   this.palette_container.find("div.eraser").click(function () {
     $("div").removeClass("selected");
     $(this).addClass("selected");
-    color = "white";
-    $("div.color_sampler").css("background-color", color);
-    
+    self.color = "white";
+    $("div.color_sampler").css("background-color", self.color);
   });
 
-  //Color dropper event listener
+  //Listens for a click on the grid and drops the currently selected color on it
   this.grid.find("div.pixel_square").click(function () {
-    $(this).css('background-color', color);
+    $(this).css('background-color', self.color);
   });
-}
 
-//This method listens for the click on the "clear all button"
-//If user confirms clear all, colors on pixels are reset
-PixelPainter.prototype.clearAll = function () {
-
-  //Clear All event listener
+  //Listens for the click on the "clear all button"
+  //If user confirms clear all, colors on pixels are reset
   this.palette_container.find("button.clear").click(function () {
-  
     var answer = confirm("Are you sure you want to clear all?")
     if(answer){//yes
-      $("div.pixel_square").css('background-color', "#fff");
+      $("div.pixel_square"
+        ).css('background-color', "#fff");
     }
   });
-}
 
+}
 
 //Extra Functionality
 //Setting cell size
-
 PixelPainter.prototype.cellSize = function (size) {
   
   this.size = size;
+  this.grid.find("div.pixel_square").css({
+    "width": this.size + "px",
+    "height": this.size + "px"
+  });
 
 }
 
--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var main = function(){
 
+  $("#container").addClass("clearfix");
   //when page loads, load the grid
   var pixelPainter = new PixelPainter(15,15);
-  pixelPainter.cellSize(40);
+  // pixelPainter.cellSize(30);
   $("#container").append(pixelPainter.render());
 
 };
